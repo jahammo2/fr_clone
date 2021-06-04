95261dc06c55b9a03eb44d56897578af5fce5fc6
Jordan Hammond <jahammo2@gmail.com>
2021-06-04 12:19:20 -0500

    version update

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule FrClone.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.3.0"}
+      {:plug_cowboy, "~> 2.0"}
+      {:cowlib, "~> 2.8.0"}
     ]
   end
 
